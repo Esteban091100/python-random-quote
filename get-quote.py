19abb5d4f621a9079b57fa9a70be132ddbf6ccc7
Esteban091100 <estebannaranjo699@gmail.com>
2021-03-04 21:58:17 -0500

    Quote changed to last and randomize lines

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,17 @@
+import random 
 def main():
-   print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-   print("quotes")
+  print(quotes[0])
+  print("Ahora si")
+  last = 13
+  rnd = random.randint(0, last)
+  last = len(quotes) -1
+  print(quotes[rnd])
+  print(quotes[13])
 
 if __name__== "__main__":
   main()
